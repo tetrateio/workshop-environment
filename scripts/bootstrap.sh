70a56f2a49cc1d994c5e986107c49ba319b95fbd
Adam Zwickey <adam@tetrate.io>
2022-01-12 09:38:08 -0500

    initial creation of working repo

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+echo "******************************"
+echo "Bootstrapping Mgmt Cluster"
+echo "******************************"
+: ${ARGO_PWD?"Need to set ARGO_PWD environment variable"}
+
+#Install ArgoCD
+kubectl create ns argocd
+helm repo add argo https://argoproj.github.io/argo-helm
+echo "argopwd: $ARGO_PWD"
+export ARGO_PWD_ENCODE=$(htpasswd -nbBC 10 "" $ARGO_PWD | tr -d ':\n' | sed 's/$2y/$2a/')
+echo "argopwd encoded: $ARGO_PWD_ENCODE"
+helm install argocd argo/argo-cd -f helm-values/argocd-values.yaml -n argocd \
+     --set configs.secret.argocdServerAdminPassword=$ARGO_PWD_ENCODE 
+while kubectl get svc argocd-server -n argocd | grep pending | wc -l | grep 0 ; [ $? -ne 0 ]; do
+    echo ArgoCD IP not assigned
+    sleep 5
+done
+ARGO_IP=$(kubectl get svc argocd-server -n argocd -o json --output jsonpath='{.status.loadBalancer.ingress[0].ip}')  
+echo "ArgoCD accessible via https://$ARGO_IP, admin/$ARGO_PWD"
+sleep 10
+argocd login $ARGO_IP --username admin --password $ARGO_PWD
